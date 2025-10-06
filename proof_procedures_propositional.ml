5855efb6132e28bc5ba2bd55a999e512c19185f2
Lado Turmanidze <48657340+Stochastic-Batman@users.noreply.github.com>
2025-10-06 21:20:49 +0400

    Added default launch function for proof_procedures_propositional.ml
    It should not run default launch function from nf.ml

--- proof_procedures_propositional.ml
+++ proof_procedures_propositional.ml
@@ -8,3 +8,5 @@ let resolution_preprocessing e = cnf (negate_propositional_expr e)
 
 let run_proof_procedures_propositional_tests () =
         Printf.printf "Preprocessed %s: %s\n" (string_of_propositional_expr expr4) (string_of_propositional_expr (resolution_preprocessing expr4))
+
+let _ = run_proof_procedures_propositional_tests ()
